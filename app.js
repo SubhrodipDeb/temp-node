392a9fb6fb3bb4cfeed06bad77244886906f08e4
Subhrodip Deb <subhrodipdeb45@gmail.com>
2022-03-02 09:01:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//GLOBALS -NO WINDOW
+// __dirname -path to the current directory
+// __filename -file name
+// require -function to use modules(CommomJs)
+// module-info about a current mdule(file)
+// process -info about environment where program is being executed
+console.log(__dirname)
+setInterval(() => {
+    console.log('hello world')
+},1000)
